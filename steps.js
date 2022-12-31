5ad939592fd1968a22741a980c276e8a156dccb7
S-M-ZUBAYER <smzubayer18@gmail.com>
2022-12-31 20:29:34 +0600

    Delete unnecessary part

--- steps.js
+++
@@ -1,7 +0,0 @@
-// Post-----
-// 1. Create Post API.
-// 2.use fetch with method post
-// 3.add header to the fetch .. 'content-type':'Application/json'
-// 4.add body to the fetch and send data
-//5.make sure to use JSON.stringify to send data
-//6. user express.json() as a middle ware
